2f0b6fd4664b24a5294de67c714510d9dc2c4278
Chiahsuan06 <chiahsuan56566498@gmail.com>
2021-07-13 15:07:36 +0800

    加入按鈕

--- 0713TryGithub/0713TryGithub/Form1.Designer.cs
+++ 0713TryGithub/0713TryGithub/Form1.Designer.cs
@@ -36,10 +36,11 @@ namespace _0713TryGithub
             // 
             this.button1.Location = new System.Drawing.Point(56, 62);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(75, 91);
             this.button1.TabIndex = 0;
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // Form1
             // 
--- 0713TryGithub/0713TryGithub/Form1.cs
+++ 0713TryGithub/0713TryGithub/Form1.cs
@@ -16,5 +16,10 @@ namespace _0713TryGithub
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Test");
+        }
     }
 }
